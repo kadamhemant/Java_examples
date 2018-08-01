0ba5604828d9b2d9506834f4de8e9fb4322c9ef3
hemant.kadam <kadamhemant53@gmail.com>
2018-08-01 15:04:54 -0400

    updated example with Threadpool

--- src/com/examples/testclass.java
+++ src/com/examples/testclass.java
@@ -2,6 +2,9 @@ package com.examples;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class testclass {
 
@@ -10,21 +13,33 @@ public class testclass {
         Path[] outFiles = {Paths.get("file1.out.txt"), Paths.get("file2.out.txt"), Paths.get("file3.out.txt")};
 
         //System.out.println(Paths.get("\\test\\resource\\file1.txt"));
+
+        ExecutorService es=Executors.newFixedThreadPool(3);
+
+
         Thread[] threads=new Thread[inFiles.length];
 
             for (int i = 0; i < inFiles.length; i++) {
                 Adder adder = new Adder(inFiles[i], outFiles[i]);
-                threads[i]=new Thread(adder);
-                threads[i].start();
+                es.submit(adder);
+                /*threads[i]=new Thread(adder);
+                threads[i].start();*/
             }
 
-        for (Thread thread:threads) {
+
+        try {
+            es.shutdown();
+            es.awaitTermination(60,TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        /*for (Thread thread:threads) {
             try {
                 thread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
 
     }
 }
